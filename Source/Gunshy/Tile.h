47fcef466c84557242c04434ebd3d9e5544d9466
Chris Nickerson <christophern@nait.ca>
2019-06-11 16:18:41 -0600

    Created the base for Tiles

---
+++ Source/Gunshy/Tile.h
@@ -0,0 +1,20 @@
+// Created 2019 by Chris Nickerson. This game is intended to be free and therefore the code is not restricted.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "Tile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNSHY_API ATile : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+
+protected:
+
+};
