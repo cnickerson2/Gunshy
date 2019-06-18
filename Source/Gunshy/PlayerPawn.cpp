579bda87c7cf7db45464f0c88550b3e33f8ea404
Chris Nickerson <cnickerson2@me.com>
2019-06-18 16:30:31 -0600

    Set up PlayerPawn to set the PlayerCamera programmatically

--- Source/Gunshy/PlayerPawn.cpp
+++ Source/Gunshy/PlayerPawn.cpp
@@ -3,20 +3,35 @@
 
 #include "PlayerPawn.h"
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 
-// Sets default values
 APlayerPawn::APlayerPawn()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
     PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Player Camera"));
+    PlayerCamera->RelativeLocation = FVector(0, 0, 300); // Three meters up
+    PlayerCamera->RelativeRotation = FRotator(0, -90, 0); // Look down
+    PlayerCamera->SetProjectionMode(ECameraProjectionMode::Orthographic);
 
 }
 
-// Called to bind functionality to input
 void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+    // Bind clicks
+    PlayerInputComponent->BindAction("Click", IE_Pressed, this, &APlayerPawn::Select);
+
+
 }
+
+void APlayerPawn::Select()
+{
+    // Cast a ray from the Click position (either mouse or touch) downwards to see if you hit a tile
+
+        // Tell the tile that it was selected
+
+}
+
 
--- Source/Gunshy/PlayerPawn.h
+++ Source/Gunshy/PlayerPawn.h
@@ -6,24 +6,32 @@
 #include "GameFramework/Pawn.h"
 #include "PlayerPawn.generated.h"
 
-
+#pragma region Forward Declarations
 class UCameraComponent;
+class UInputComponent;
+#pragma endregion Forward Declarations
 
 UCLASS()
 class GUNSHY_API APlayerPawn : public APawn
 {
 	GENERATED_BODY()
+#pragma region Fields and Properties
+protected:
+    // Default Player Camera
+    UPROPERTY(Category = "Camera", BlueprintReadOnly)
+        UCameraComponent * PlayerCamera = nullptr; // TODO: Figure out the bug here where VisibleAnywhere is allowing this to be edited...
+#pragma endregion Fields and Properties
 
+#pragma region Methods
 public:
 	// Sets default values for this pawn's properties
 	APlayerPawn();
 
 	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
 protected:
-    // Default Player Camera
-    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category="Camera")
-    UCameraComponent * PlayerCamera = nullptr;
-
+    // Attempt to select a tile
+    virtual void Select();
+#pragma endregion Methods
 };
